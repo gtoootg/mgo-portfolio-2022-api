12743fd9799344328d0b1f6d86f599e5bb586b7b
michihiro.goto <michihiro.goto@techpilot.net>
2023-11-30 09:40:17 +0100

    halfway through tutorial

--- src/main/java/com/mgoportfolio2022api/mgoportfolio2022api/security/JWTAuthenticationFilter.java
+++ src/main/java/com/mgoportfolio2022api/mgoportfolio2022api/security/JWTAuthenticationFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +15,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@RequiredArgsConstructor;
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
   @Autowired
@@ -20,9 +23,30 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
   @Autowired
   private  CustomUserDetailsService customUserDetailsService;
 
+  private final JwtService jwtService;
+
+  public JWTAuthenticationFilter() {
+  }
+
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-      FilterChain filterChain) throws ServletException, IOException {
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain
+  ) throws ServletException, IOException {
+    final  String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String userEmail;
+    if(authHeader == null||!authHeader.startsWith("Bearer ")){
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    jwt = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(jwt);
+
+
+
 
     String token = getJWTFromRequest(request);
 
