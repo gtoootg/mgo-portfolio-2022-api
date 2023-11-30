7f9766d4ab13877e1ff97a74f0a1f73fb8b92a6a
michihiro.goto <michihiro.goto@techpilot.net>
2023-11-30 10:54:11 +0100

    Revert "Revert "halfway through tutorial""
    This reverts commit 32a507ebfdc79db1f7e70d845f0e0556b47b7f0d.

---
+++ src/main/java/com/mgoportfolio2022api/mgoportfolio2022api/security/JwtService.java
@@ -0,0 +1,19 @@
+package com.mgoportfolio2022api.mgoportfolio2022api.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+  public String extractUsername(String token) {
+    return null;
+  }
+
+  private Claims extractAllClaims(String token){
+    return Jwts
+
+  }
+}
