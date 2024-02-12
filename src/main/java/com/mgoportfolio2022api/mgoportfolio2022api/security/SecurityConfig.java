0e0206b22258f13874410c3e84656e826f2e151a
michihiro.goto <michihiro.goto@techpilot.net>
2024-02-12 11:49:25 +0100

    now work

--- src/main/java/com/mgoportfolio2022api/mgoportfolio2022api/security/SecurityConfig.java
+++ src/main/java/com/mgoportfolio2022api/mgoportfolio2022api/security/SecurityConfig.java
@@ -76,7 +76,8 @@ public class SecurityConfig {
     configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
 
     configuration.setAllowedHeaders(List.of("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT"
+        ));
     configuration.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
